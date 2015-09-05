e52ac1b4f62ab95ce714198b547726a1f741a483
Emati Mitame <emati@mita.me>
2015-09-05 18:55:14 +0100

    Added support for PyPy3

--- runserver.py
+++ runserver.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 from bitorb import app
 from bitorb.config import config
-from ssl import SSLContext, PROTOCOL_TLSv1_2
+from ssl import SSLContext
+import threading
+import time
 from threading import Thread
+
+
+# Pypy compatability
+try:
+    from ssl import PROTOCOL_TLSv1_2 as PROTOCOL_TLSv1
+except ImportError:
+    from ssl import PROTOCOL_TLSv1 as PROTOCOL_TLSv1
 
 run_http = config["insecure"]["enabled"]
 run_https = config["secure"]["enabled"]
 
 if run_https:
-    context = SSLContext(PROTOCOL_TLSv1_2)
+    context = SSLContext(PROTOCOL_TLSv1)
     context.load_cert_chain(
         config["secure"]["cert"],
         config["secure"]["key"]
@@ -17,7 +26,6 @@ if run_https:
 if run_http and run_https:
     if config["debug"]:
         raise Warning("Cannot run in debug mode with both https and http enabled due to flask limitations.")
-
     Thread(
         target=app.run,
         kwargs={
